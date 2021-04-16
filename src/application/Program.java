c2014205b05afc4c26389ca9e3c4dae46a077363
Ítalo Martins de Deus <italomddeus@gmail.com>
2021-04-16 10:56:29 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main (String [] args) {
 		
+		Locale.setDefault(Locale.US);
+		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Bob", "bob@gmail.com", LocalDate.now(), 2400.0, obj);
+		System.out.println(seller);
 		
 	}
 
